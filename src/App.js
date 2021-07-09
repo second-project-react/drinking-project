6ef6efa6c2d06bf73745506cdd2bce2d8c82eddf
Pallavi <pallavid0808@gmail.com>
2021-07-09 17:51:05 +0200

    First Test Successful

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
-import './App.css';
+import React from 'react'
+import './App.css'
+import Slider1 from './components/Slider1/Slider1.js'
+import Axios from 'axios'
+
 
 function App() {
+
+  const getData = () => {
+    Axios.get('https://www.thecocktaildb.com/api/json/v2/9973533/list.php?c=list')
+    .then((response)=>{
+     console.log(response)
+     return response
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+  
   return (
-    <div className="App">
-      <h1>Our second project Cocktails</h1>
+    <div  className="App">
+
+      <h1>Our Second Project Cocktails</h1>
+
+      <Slider1 data= {getData}/>
+
     </div>
-  );
+  )
 }
 
 export default App;
