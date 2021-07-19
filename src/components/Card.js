50fe7d166876c86a6a2ad37a6efb7072c812f8d6
Pallavi <pallavid0808@gmail.com>
2021-07-19 18:56:35 +0200

    Added some css

---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+
+import Axios from 'axios'
+import React, {  useState } from 'react';
+
+const Card2 = () => {
+   
+  
+  
+   const element = () => {
+      Axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+      .then((response)=>{
+         setRandom(response.data.drinks[0])
+         return response.data.drinks[0]
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+   }
+   const [random, setRandom] = useState(getRandom)
+
+   
+   console.log(random);
+   return(
+      <div className="surpriseWrapper">
+         <div className="surpriseTitleWrapper">
+            <h2 className="surpriseTitle">Surprise Me!</h2>
+            <div className="surpriseRefresh" onClick={getRandom}><RefreshCcw size={30}/></div>
+         </div>
+         <div className="surpriseImg">
+            {/* <img src='/assets/img/c.png' alt="" /> */}
+
+            <img src={random?.strDrinkThumb} alt="random cocktail" />
+         </div>
+         <div  className="surpriseDesc">
+            <h3>{random?.strDrink}</h3>
+         </div>
+      </div>
+   );
+}
+export default Surprise
--- src/components/Card1.js
+++ src/components/Card1.js
@@ -1,32 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-
+import './Card1.css';
 
 function Card1(){
 
-   const [value, setValue]= useState([])
+    const [value, setValue]= useState({})
+  
+    useEffect(()=>{
+       Axios.get('https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=15300')
+            .then((response) => response.data)
+         .then((data)=>{
+             setValue(data.drinks[0])
+             console.log(data.drinks[0])
+         })
+         .catch((error)=>{
+           console.log(error)
+         })
+     }, [])
  
-   useEffect(()=>{
-      Axios.get('https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=15300')
-           .then((response) => response.data)
-        .then((data)=>{
-            setValue(data)
-            console.log(data)
-        })
-        .catch((error)=>{
-          console.log(error)
-        })
-    }, [])
-
+     const arrayOfIngredients=[]; 
+     for(let i=1; i<=15; i++){
+        arrayOfIngredients.push(value['strIngredient'+i])
+      }
+      console.log(arrayOfIngredients)
+      const arrayOfMeasure=[]; 
+     for(let i=1; i<=15; i++){
+        arrayOfMeasure.push(value['strMeasure'+i])
+      }
+      
+    const finalArrayOfIngredients = arrayOfIngredients
+                                      .filter((item) => item!==null &&  item !==''  &&  item !== undefined)
+                                      .map((item) => item)
+     
+    const finalArrayOfMeasure = arrayOfMeasure
+                                      .filter((item) => item!==null &&  item !==''  &&  item !== undefined)
+                                      .map((item) => item)
     
-    return(
-        <div>
-            <h1>Hellloooooo</h1>
-            <h2>{value.drinks[0].strDrink}</h2>
-            <img src={value.drinks[0].strDrinkThumb}></img>
-          <p>{value.drinks[0].strInstructions}</p>
-        </div>)
-    
-}
 
+    let allLiOfIngridientsAndMeasure = [];
+
+    const twoInOne = ((item, index) => allLiOfIngridientsAndMeasure.push(`${finalArrayOfMeasure[index]} - ${item} `))
+    
+    finalArrayOfIngredients.forEach(twoInOne);
+
+     return(
+         <div className="Card1">
+           <h2 className="drinkName">{value.strDrink}</h2>
+           <h5 className="drinkCategory">{value.strAlcoholic}</h5>
+            <h5 className="drinkCategory">{value.strTags}</h5>
+
+           <div className="container1">
+            <div className="container1A">
+              <img className="drinkImage" src={value.strDrinkThumb} alt="img"></img>
+           </div>
+           <div className="container1B">
+             
+             <h3 className="sectionName">INGREDIENTS</h3>
+             <ul className="drinkIngredients">
+              {allLiOfIngridientsAndMeasure.map((item) => <li>{item}</li>)}
+               </ul>
+             <h3 className="sectionName">INSTRUCTIONS</h3>
+             <p className="drinkInstructions">{value.strInstructions}</p>
+           </div>
+           </div>
+             
+         </div>)
+     
+ }
+ 
 export default Card1;
