091fff237735cfd750c262a6dcb37d622466fcfd
Pallavi <pallavid0808@gmail.com>
2021-07-29 11:02:27 +0200

    copied some

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { useState } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import Home from './Home';
+import Sidebar from './components/sidebar/Sidebar';
 
-import Sidebar from './components/Sidebar';
-import Navigation from './components/Navbar'
+// Pages
+// // MainPage
+import Home from './Home';
+// // Categories
 import ShotsList from './components/mainCarousel/Shots'
 import MocktailsList from './components/mainCarousel/Mocktails'
 import CreamyList from './components/mainCarousel/Creamy'
 import PunchList from './components/mainCarousel/PunchList';
 import ClassicsList from './components/mainCarousel/ClassicsList'
-import Latest from './components/Latest'
+// // Spirits
+import Bourbon from './components/pages/spirits/Bourbon'
+import Brandy from './components/pages/spirits/Brandy'
+import Gin from './components/pages/spirits/Gin'
+import Liqueur from './components/pages/spirits/Liqueur'
+import Rum from './components/pages/spirits/Rum'
+import Scotch from './components/pages/spirits/Scotch'
+import Tequila from './components/pages/spirits/Tequila'
+import Vodka from './components/pages/spirits/Vodka'
 
-import ModalDisplay from './components/mainCarousel/ModalDisplay'
 
-import {Container} from "react-bootstrap";
+
+
+
+
 
 
 import Ingredient from './components/Ingredient';
@@ -28,49 +39,32 @@ function App() {
   return (
 
     <Router>
-      <Container fluid pr-0>
-
-          <div className="content-wrapper">
-
-               <div id="sidebar-wrapper">      
-
-                 <Sidebar />
-
-            
-
-               </div>
-
-          <div id="page-content-wrapper">
-
-          <Navigation/>
-          <Switch>
+      <div className='App'>
+          <Sidebar />
+          <Switch>          
             <Route path='/' exact component={Home} />
-          {/* Sidebar */}
-          {/* <Route path='/spirits' component={Spirits} />
-          <Route path='/multi-ingridient' component={MultiIngridient} />
-          <Route path='/popular' component={Popular} />          */}
-
+            {/* Categories */}
             <Route path='/shots' exact component={ShotsList} />
-        
-         
             <Route path='/mocktails' exact component={MocktailsList} />
-          
-            
-            <Route path='/creamy' exact component={CreamyList}/>
-        
+            <Route path='/creamy' exact component={CreamyList} />
+            <Route path='/classics' exact component={ClassicsList} />
+            <Route path='/punch' exact component={PunchList} />
+            {/* Spirits */}
+            <Route path='/bourbon' exact component={Bourbon} />
+            <Route path='/brandy' exact component={Brandy} />
+            <Route path='/gin' exact component={Gin} />
+            <Route path='/liqueur' exact component={Liqueur} />
+            <Route path='/rum' exact component={Rum} />
+            <Route path='/scotch' exact component={Scotch} />
+            <Route path='/tequila' exact component={Tequila} />
+            <Route path='/vodka' exact component={Vodka} />
 
-            <Route path='/classics' exact component={ClassicsList}/>
-         
+            <Route path='/multi-ingredients' exact component={Ingredient} />
 
-            <Route path='/punch' exact component={PunchList}/>
 
-            <Route path='/latest' exact component={Latest}/>
-          
           </Switch>
-          </div>
-          </div>
 
-       </Container>
+       </div>
    </Router>
   )
 
