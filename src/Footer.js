0bf33f4e747107a76b572ac2cfdbb0e8d5dd25ba
Pallavi <pallavid0808@gmail.com>
2021-08-25 17:45:47 +0200

    some css changes

--- src/Footer.js
+++ src/Footer.js
@@ -1,112 +1,144 @@
 import './Footer.css';
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const Footer = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [thanks, setThanks] = useState("Drop a Message")
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [message, setMessage] = useState('');
+   const [thanks, setThanks] = useState('Drop a Message');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("CLICK", email);
-    setName("");
-    setEmail("");
-    setMessage("");
-    setThanks("Thanks A Lot 😊");
-  };
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('CLICK', email);
+      setName('');
+      setEmail('');
+      setMessage('');
+      setThanks('Thanks A Lot 😊');
+   };
 
-  return (
-   
-    <div className="footerContainer">
-        
-      <div className="nameContainer">
-        <div className="socialContainer">
-          <p>Alexej Holad</p>
-          <a href="https://github.com/AlexHolad" target="new">
-            <img className="socialIconSmall" src="./img/github1.png" alt="" />
-          </a>
-          <a href="https://www.linkedin.com/in/alexejholad/" target="new">
-            <img className="socialIconSmall" src="./img/linkedin2.png" alt="" />
-          </a>
-        </div>
-        <div className="socialContainer">
-          <p>Sasmitha Nagesh</p>
-          <a href="https://github.com/Sashmu" target="new">
-            <img className="socialIconSmall" src="./img/github1.png" alt="" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/sasmitha-nagesh-9818336b/"
-            target="new"
-          >
-            <img className="socialIconSmall" src="./img/linkedin2.png" alt="" />
-          </a>
-        </div>
-        <div className="socialContainer">
-          <p>Pallavi Das</p>
-          <a href="https://github.com/pallavid08" target="new">
-            <img className="socialIconSmall" src="./img/github1.png" alt="" />
-          </a>
-          <a href="https://www.linkedin.com/in/das-pallavi/" target="new">
-            <img className="socialIconSmall" src="./img/linkedin2.png" alt="" />
-          </a>
-        </div>
+   return (
+      <div className="footerContainer">
+         <div className="nameContainer">
+            <div className="socialContainer">
+               <p>Alexej Holad</p>
+               <a href="https://github.com/AlexHolad" target="new">
+                  <img
+                     className="socialIconSmall"
+                     src="./img/github1.png"
+                     alt=""
+                  />
+               </a>
+               <a href="https://www.linkedin.com/in/alexejholad/" target="new">
+                  <img
+                     className="socialIconSmall"
+                     src="./img/linkedin2.png"
+                     alt=""
+                  />
+               </a>
+            </div>
+            <div className="socialContainer">
+               <p>Sasmitha Nagesh</p>
+               <a href="https://github.com/Sashmu" target="new">
+                  <img
+                     className="socialIconSmall"
+                     src="./img/github1.png"
+                     alt=""
+                  />
+               </a>
+               <a
+                  href="https://www.linkedin.com/in/sasmitha-nagesh-9818336b/"
+                  target="new"
+               >
+                  <img
+                     className="socialIconSmall"
+                     src="./img/linkedin2.png"
+                     alt=""
+                  />
+               </a>
+            </div>
+            <div className="socialContainer">
+               <p>Pallavi Das</p>
+               <a href="https://github.com/pallavid08" target="new">
+                  <img
+                     className="socialIconSmall"
+                     src="./img/github1.png"
+                     alt=""
+                  />
+               </a>
+               <a href="https://www.linkedin.com/in/das-pallavi/" target="new">
+                  <img
+                     className="socialIconSmall"
+                     src="./img/linkedin2.png"
+                     alt=""
+                  />
+               </a>
+            </div>
+         </div>
+
+         <div className="centerContainer">
+            <div className="iconsContainer">
+               <div>
+                  <p>Technologies</p>
+               </div>
+               <div className="iconsWrapper">
+                  <img className="footerIcons" src="./img/html1.png" alt="" />
+                  <img className="footerIcons" src="./img/css1.png" alt="" />
+                  <img
+                     className="footerIcons"
+                     src="./img/js-format.png"
+                     alt=""
+                  />
+                  <img className="footerIcons" src="./img/atom.png" alt="" />
+               </div>
+            </div>
+            <div className="madeWith">
+               <p>
+                  <span>Made with ♥︎ by Students</span>{' '}
+                  <span>at Wild Code School</span>
+               </p>
+            </div>
+         </div>
+
+         <div className="formContainer">
+            <div className="allLabels">
+               <form
+                  action=""
+                  method="POST"
+                  className="formInputWrapper"
+                  onSubmit={handleSubmit}
+               >
+                  <div>{thanks}</div>
+
+                  <input
+                     className="inputBox"
+                     type="text"
+                     placeholder="Name"
+                     value={name}
+                     onChange={(e) => setName(e.target.value)}
+                  />
+
+                  <input
+                     className="inputBox"
+                     type="email"
+                     placeholder="Email"
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}
+                  />
+
+                  <input
+                     className="inputBox"
+                     type="text"
+                     placeholder="Message"
+                     style={{ height: '50px' }}
+                     value={message}
+                     onChange={(e) => setMessage(e.target.value)}
+                  />
+
+                  <button type="submit">Send</button>
+               </form>
+            </div>
+         </div>
       </div>
-
-      <div className="centerContainer">
-        <div className="iconsContainer">
-          <div>
-            <p>Technologies</p>
-          </div>
-          <div className="iconsWrapper">
-            <img className="footerIcons" src="./img/html1.png" alt="" />
-            <img className="footerIcons" src="./img/css1.png" alt="" />
-            <img className="footerIcons" src="./img/js-format.png" alt="" />
-            <img className="footerIcons" src="./img/atom.png" alt="" />
-          </div>
-        </div>
-        <div className="madeWith">
-          <p>
-            <span>Made with ♥︎ by Students</span>{" "}
-            <span>at Wild Code School</span>
-          </p>
-        </div>
-      </div>
-
-      <div className="formContainer">
-        
-        <div className="allLabels">
-          <form className="formInputWrapper" onSubmit={handleSubmit}>
-            <div>{thanks}</div>
-            <input
-              className="inputBox"
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-
-            <input
-              className="inputBox"
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-
-            <input
-              className="inputBox"
-              type="text"
-              placeholder="Message"
-              style={{ height: "50px" }}
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <button type="submit">Send</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+   );
 };
 export default Footer;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,116 +1,116 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import * as AiIcons from 'react-icons/ai'
-import * as FaIcons from 'react-icons/fa'
-import { SidebarData } from './SidebarData'
-import Submenu from './SubMenu'
-import { IconContext } from 'react-icons/lib'
-import './Sidebar.css'
-import Searchbar from '../Searchbar'
-
-
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import * as AiIcons from 'react-icons/ai';
+import * as FaIcons from 'react-icons/fa';
+import { SidebarData } from './SidebarData';
+import Submenu from './SubMenu';
+import { IconContext } from 'react-icons/lib';
+import './Sidebar.css';
+import Searchbar from '../Searchbar';
 
 const Nav = styled.div`
-    width: 100vw;
-    background: white;
-    position: fixed;
-    top: 0;
-    z-index: 10;
-    height: 80px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
+   width: 100vw;
+   background: white;
+   position: fixed;
+   top: 0;
+   z-index: 10;
+   height: 80px;
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
 `;
-
 
 const NavIcon = styled(Link)`
-    margin-left: 2rem;
-    font-size: 2rem;
-    height: 80px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
+   margin-left: 2rem;
+   font-size: 2rem;
+   height: 80px;
+   display: flex;
+   justify-content: flex-start;
+   align-items: center;
 
-
-    svg{
-    color:black;
-     
-    }
+   svg {
+      color: black;
+   }
 `;
 
-
 const SidebarNav = styled.nav`
-    background: #4C5760;
-    width: 250px;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    position: fixed;
-    top: 0;
-    left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
-    transition: 350ms;
-    z-index: 12;
-    opacity: 0.9;
-
+   background: #4c5760;
+   width: 250px;
+   height: 100vh;
+   display: flex;
+   justify-content: center;
+   position: fixed;
+   top: 0;
+   left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+   transition: 350ms;
+   z-index: 12;
+   opacity: 0.9;
 `;
 
 const SidebarWrap = styled.div`
-    width: 100%;
+   width: 100%;
 `;
 // const pageName = styled.h1`
 //   color: blue;
 // `;
 const PageWrapper = styled.div`
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    z-index:11;
-    background-color: black;
-    opacity:0.4;
-    top: 0;
+   width: 100vw;
+   height: 100vh;
+   position: fixed;
+   z-index: 11;
+   background-color: black;
+   opacity: 0.4;
+   top: 0;
 `;
 
 const Sidebar = () => {
-    const [sidebar, setSidebar] = useState(false)
+   const [sidebar, setSidebar] = useState(false);
 
-    const ShowSidebar = () => setSidebar(!sidebar)
+   const ShowSidebar = () => setSidebar(!sidebar);
 
-
-    return (
-        <>
-        
-        <IconContext.Provider value={{ color: '#70C9B1'}}>
+   return (
+      <>
+         <IconContext.Provider value={{ color: '#70C9B1' }}>
             <Nav>
-                
-                <NavIcon>
-                    <FaIcons.FaBars onClick={ShowSidebar}/>
-                    </NavIcon> 
-                    <Link to='/'><p className="pageName">The CockTail Bar</p></Link>
-                    <Link to='/'>
-                    <div className="logoDiv">
-                    <img className="logo" src="./img/logo.png"  alt="drink" />  
-                    </div>
-                    </Link>
-                    <Searchbar />
-                     
+               <div className="pageNameAndLogo">
+                  <NavIcon>
+                     <FaIcons.FaBars onClick={ShowSidebar} />
+                  </NavIcon>
+                  {/* <div className="pageNameAndSearchContainer"> */}
+
+                  <Link to="/">
+                     <p className="pageName">The CockTail Bar</p>
+                  </Link>
+                  <Link to="/">
+                     <div className="logoDiv">
+                        <img
+                           className="logo"
+                           src="./img/logo.png"
+                           alt="drink"
+                        />
+                     </div>
+                  </Link>
+               </div>
+               {/* <div className="searchComponent"> */}
+               <Searchbar />
+               {/* </div> */}
+               {/* </div> */}
             </Nav>
             <SidebarNav sidebar={sidebar}>
-                <SidebarWrap>
-                <NavIcon>
-                    <AiIcons.AiOutlineClose onClick={ShowSidebar}/>
-                </NavIcon>
-                {SidebarData.map((item, index) => {
-                    return (
-                        <Submenu item={item} key={index}/>
-                    )
-                })}
-                </SidebarWrap>
-                </SidebarNav>
-                {sidebar && <PageWrapper onClick={ShowSidebar}></PageWrapper>}
-        </IconContext.Provider>    
-        </>
-    )
-}
+               <SidebarWrap>
+                  <NavIcon>
+                     <AiIcons.AiOutlineClose onClick={ShowSidebar} />
+                  </NavIcon>
+                  {SidebarData.map((item, index) => {
+                     return <Submenu item={item} key={index} />;
+                  })}
+               </SidebarWrap>
+            </SidebarNav>
+            {sidebar && <PageWrapper onClick={ShowSidebar}></PageWrapper>}
+         </IconContext.Provider>
+      </>
+   );
+};
 
-export default Sidebar
+export default Sidebar;
